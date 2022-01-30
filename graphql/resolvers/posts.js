e676322aded486d6b691c707a8d024f0183bc3c5
rbroda <brodaryan@gmail.com>
2022-01-30 18:49:37 -0500

    added comment and like functionality

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
@@ -55,6 +55,28 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    likePost: async (_, { postId }, context) => {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //  Post already liked, unlike it.
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          //  Post not liked yet, like it.
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
